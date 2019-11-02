a0f4444115f6f905f9c4f24c9cfc3339a19a7014
nikachapo <44478420+nikachapo@users.noreply.github.com>
2019-11-02 20:57:50 +0400

    commit

--- app/src/main/java/com/example/jobs/BottomSheet.java
+++ app/src/main/java/com/example/jobs/BottomSheet.java
@@ -2,6 +2,8 @@ package com.example.jobs;
 
 
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +12,12 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
 
 public class BottomSheet extends BottomSheetDialogFragment {
     private String profilePictureURL;
@@ -42,7 +47,7 @@ public class BottomSheet extends BottomSheetDialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.bottom_sheet_layout, container, false);
         Button close = v.findViewById(R.id.close_bottom_sheet);
-        ImageView profilePicture = v.findViewById(R.id.sheet_profile_image);
+        final ImageView profilePicture = v.findViewById(R.id.sheet_profile_image);
         TextView cityTextView = v.findViewById(R.id.sheet_vacancy_city);
         TextView vacancyCategoryTextView = v.findViewById(R.id.sheet_vacancy_category);
         TextView vacancyNameTextView = v.findViewById(R.id.sheet_vacancy_name);
@@ -58,11 +63,24 @@ public class BottomSheet extends BottomSheetDialogFragment {
             }
         });
 
-        Picasso.with(getContext())
-                .load(this.profilePictureURL)
-                .centerInside()
-                .fit()
-                .into(profilePicture);
+
+        Picasso.with(getContext()).load(this.profilePictureURL)
+
+                .resize(200, 200)
+                .into(profilePicture, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        Bitmap imageBitmap = ((BitmapDrawable) profilePicture.getDrawable()).getBitmap();
+                        RoundedBitmapDrawable imageDrawable = RoundedBitmapDrawableFactory.create(getResources(), imageBitmap);
+                        imageDrawable.setCircular(true);
+                        imageDrawable.setCornerRadius(Math.max(imageBitmap.getWidth(), imageBitmap.getHeight()) / 2.0f);
+                        profilePicture.setImageDrawable(imageDrawable);
+                    }
+                    @Override
+                    public void onError() {
+                        profilePicture.setImageResource(R.drawable.ic_default_company_image);
+                    }
+                });
 
         cityTextView.setText(this.city);
         vacancyCategoryTextView.setText(this.vacancyCategory);
